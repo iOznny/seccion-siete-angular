5ce2a99fb6b8e05e69040284040c3236edceb51f
iOznny <fo170014@gmail.com>
2021-01-11 13:26:20 -0600

    Avance en local storage

--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -7,10 +7,43 @@ import { List } from '../models/list.model';
 
 export class WishesService {
 
+  // Lista donde guardaremos los title y items de la misma.
   lists: List[] = [];
 
   constructor() {
-    console.log('Service inicializado.');
+    // Cargamos el storage.
+    this.loadStorage();
+
+    //const list_one = new List('Recolectar');
+    //this.lists.push(list_one);
   }
 
+  // Creamos la lista.
+  createList(title: string) {
+    const new_list = new List(title);
+    this.lists.push(new_list);
+    this.saveStorage();
+
+    return new_list.id;
+  }
+
+  // Obtener lista.
+  getList(id: string | number) {
+    id = Number(id);
+    return this.lists.find( listData => listData.id === id);
+  }
+
+  // Guardamos la nueva lista creada.
+  saveStorage() {
+    localStorage.setItem('data', JSON.stringify(this.lists) );
+  }
+
+  // Cargamos las listas almacenadas en el storage.
+  loadStorage() {
+    if(localStorage.getItem('data')) {
+      this.lists = JSON.parse(localStorage.getItem('data'));
+    }
+  }
+
+  
 }
